1f39b4486526884e29175b541dad6f4c1c4c3f96
ANMOL MITTAL <79745096+anmolm07@users.noreply.github.com>
2021-09-24 10:47:17 +0530

    Update and rename 2-IsSubset.txt to 2-IsSubset.cpp

--- array/2-IsSubset.cpp
+++ array/2-IsSubset.cpp
@@ -1,9 +1,12 @@
-Problem-Given two arrays: a1[0..n-1] of size n and a2[0..m-1] of size m. 
+/*Problem-Given two arrays: a1[0..n-1] of size n and a2[0..m-1] of size m. 
 Task is to check whether a2[] is a subset of a1[] or not. 
 Both the arrays can be sorted or unsorted. 
 It may be assumed that elements in both array are distinct.
 
+link -https://practice.geeksforgeeks.org/problems/array-subset-of-another-array2317/1
+
 solution- 
+*/
 string isSubset(int a1[], int a2[], int n, int m) {
     unordered_set<int>S;
     for(int i=0;i<n;i++)
