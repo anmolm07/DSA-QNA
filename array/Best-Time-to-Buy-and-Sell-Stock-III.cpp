48ae2d7d2aa5056186d994b8d4b8cd745ffdfd85
ANMOL MITTAL <79745096+anmolm07@users.noreply.github.com>
2021-09-24 10:49:37 +0530

    Update and rename Best Time to Buy and Sell Stock III to Best-Time-to-Buy-and-Sell-Stock-III.cpp

--- array/Best-Time-to-Buy-and-Sell-Stock-III.cpp
+++ array/Best-Time-to-Buy-and-Sell-Stock-III.cpp
@@ -1,8 +1,11 @@
-Problem - You are given an array prices where prices[i] is the price of a given stock on the ith day.
+/*Problem - You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
 Find the maximum profit you can achieve. You may complete at most two transactions.
 
+link- https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+
 Solution -
+*/
 int max(int a,int b){
         return a>b?a:b;
     }
