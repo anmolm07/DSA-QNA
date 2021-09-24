688c9f589f8ac7a20c8b7cd75546b17ffaabb387
ANMOL MITTAL <79745096+anmolm07@users.noreply.github.com>
2021-09-24 11:12:07 +0530

    Update and rename kadane to kadane.cpp

--- array/kadane.cpp
+++ array/kadane.cpp
@@ -1,5 +1,7 @@
-Problem - Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
+/* Problem - Given an array Arr[] of N integers. Find the contiguous sub-array(containing at least one number) which has the maximum sum and return its sum.
 solution - 
+Link - https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+*/
 int maxSubarraySum(int arr[], int n){
         int max_so_far=0,max=INT_MIN;
         for(int i=0;i<n;i++)
