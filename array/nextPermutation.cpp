c1e03b97cbb2e5fa8f6172831929c42a08c1130f
ANMOL MITTAL <79745096+anmolm07@users.noreply.github.com>
2021-09-24 11:14:17 +0530

    Update and rename nextPermutation to nextPermutation.cpp

--- array/nextPermutation.cpp
+++ array/nextPermutation.cpp
@@ -1,8 +1,11 @@
-'''Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+/*'''Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
 
 If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
 
 The replacement must be in place and use only constant extra memory.'''
+LINK -https://leetcode.com/problems/next-permutation/
+	
+*/
 
 
 #include <iostream>
