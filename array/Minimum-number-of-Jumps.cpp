a5a5fc0d0a16a676f7107aca633f4a32acd8835e
ANMOL MITTAL <79745096+anmolm07@users.noreply.github.com>
2021-09-24 10:59:45 +0530

    Update and rename Minimum number of Jumps to Minimum-number-of-Jumps.cpp

--- array/Minimum-number-of-Jumps.cpp
+++ array/Minimum-number-of-Jumps.cpp
@@ -1,4 +1,4 @@
- Q-Given an array of N integers arr[] where each element represents the max number of steps that can be made forward from that element. Find the minimum number of jumps to reach the end of the array (starting from the first element). If an element is 0, then you cannot move through that element.
+/* Q-Given an array of N integers arr[] where each element represents the max number of steps that can be made forward from that element. Find the minimum number of jumps to reach the end of the array (starting from the first element). If an element is 0, then you cannot move through that element.
 Note: Return -1 if you can't reach the end of the array.
 
 
@@ -8,10 +8,13 @@ Input:
 N = 11 
 arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9} 
 Output: 3 
+
+Link - https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
  
- 
+*/ 
+
+
  int minJumps(int arr[], int n){
-        // Your code here
     if(arr[0]==0) return -1;
     
     int steps=arr[0];
