6555b5514b95b089ae8902400b1aab314719efc0
S ADITYA <54898510+Adityadamen@users.noreply.github.com>
2021-09-24 10:41:49 +0530

    Update and rename diameter of tree to diameter of tree.cpp

--- Tree/diameter of tree.cpp
+++ Tree/diameter of tree.cpp
@@ -1,10 +1,10 @@
-Problem - Given a Binary Tree, find diameter of it.
-The diameter of a tree is the number of nodes on the longest path between two end nodes in the tree. 
-The diagram below shows two trees each with diameter nine, 
-the leaves that form the ends of a longest path are shaded (note that there is more than one path in each tree of length nine, but no path longer than nine nodes).
-http://geeksforgeeks.org/wp-content/uploads/tree_diameter.GIF
+//Problem - Given a Binary Tree, find diameter of it.
+//The diameter of a tree is the number of nodes on the longest path between two end nodes in the tree. 
+//The diagram below shows two trees each with diameter nine, 
+//the leaves that form the ends of a longest path are shaded (note that there is more than one path in each tree of length nine, but no path longer than nine nodes).
+//http://geeksforgeeks.org/wp-content/uploads/tree_diameter.GIF
 
-Solution - 
+//Solution - 
 class Solution {
   public:
     // Function to return the diameter of a Binary Tree.
