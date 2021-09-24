3520d950c628ccb62056802e7614d0952d8d9766
ANMOL MITTAL <79745096+anmolm07@users.noreply.github.com>
2021-09-24 11:22:11 +0530

    Update and rename Bitonic Point (geeksforgeeks)) to Bitonic-Point.cpp

--- Searching and Sorting/Bitonic-Point.cpp
+++ Searching and Sorting/Bitonic-Point.cpp
@@ -1,3 +1,13 @@
+/*
+
+Given an array arr of n elements which is first increasing and then may be decreasing, find the maximum element in the array.
+Note: If the array is increasing then just print then last element will be the maximum value.
+
+
+
+link- https://practice.geeksforgeeks.org/problems/maximum-value-in-a-bitonic-array3001/1
+*/
+
 class Solution{
 public:
 	
