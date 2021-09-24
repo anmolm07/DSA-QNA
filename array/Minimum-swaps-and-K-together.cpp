1d32ebf3767e6280ef545af54b22edca9e90b11a
ANMOL MITTAL <79745096+anmolm07@users.noreply.github.com>
2021-09-24 11:03:40 +0530

    Update and rename Minimum swaps and K together to Minimum-swaps-and-K-together.cpp

--- array/Minimum-swaps-and-K-together.cpp
+++ array/Minimum-swaps-and-K-together.cpp
@@ -1,5 +1,11 @@
+/*
 Problem - Given an array of n positive integers and a number k. Find the minimum number of swaps required to bring all the numbers less than or equal to k together.
+
+Link - https://practice.geeksforgeeks.org/problems/minimum-swaps-required-to-bring-all-elements-less-than-or-equal-to-k-together4847/1
 solution - 
+ 
+    
+ */
 int minSwap(int *arr, int n, int k) {
     int good=0,bad=0;
     for(int i=0;i<n;i++)
