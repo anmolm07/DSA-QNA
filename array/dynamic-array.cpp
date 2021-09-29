19fb561cfac97a3b8d52b7b7d4de0d8132dfcaf9
ANMOL MITTAL <79745096+anmolm07@users.noreply.github.com>
2021-09-29 14:09:36 +0530

    Create dynamic-array.cpp

---
+++ array/dynamic-array.cpp
@@ -0,0 +1,140 @@
+/*
+Declare a 2-dimensional array,arr, of n  empty arrays. All arrays are zero indexed.
+Declare an integer, last, and initialize it to .
+
+There are  types of queries, given as an array of strings for you to parse:
+
+Query: 1 x y
+Let .
+Append the integer  to .
+Query: 2 x y
+Let .
+Assign the value  to .
+Store the new value of  to an answers array.
+
+Link - https://www.hackerrank.com/challenges/dynamic-array/problem
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'dynamicArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+vector<int> dynamicArray(int n, vector<vector<int>> queries) {
+    vector<vector<int>>arr(n);
+    vector<int>b;
+    int last=0;
+    for(int i=0;i<queries.size();i++)
+    {
+        if(queries[i][0]==1)
+        {
+            int idx=(queries[i][1]^last)%n;
+            arr[idx].push_back(queries[i][2]);
+        }
+        else{
+            int idx=(queries[i][1]^last)%n;
+            int pos=queries[i][2]%arr[idx].size();
+            last=arr[idx][pos];
+            b.push_back(last);
+        }
+    }
+    return b;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int q = stoi(first_multiple_input[1]);
+
+    vector<vector<int>> queries(q);
+
+    for (int i = 0; i < q; i++) {
+        queries[i].resize(3);
+
+        string queries_row_temp_temp;
+        getline(cin, queries_row_temp_temp);
+
+        vector<string> queries_row_temp = split(rtrim(queries_row_temp_temp));
+
+        for (int j = 0; j < 3; j++) {
+            int queries_row_item = stoi(queries_row_temp[j]);
+
+            queries[i][j] = queries_row_item;
+        }
+    }
+
+    vector<int> result = dynamicArray(n, queries);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
