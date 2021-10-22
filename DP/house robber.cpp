61e2ab78bc674088efb0f1619f69c94be322e449
asiskaur <54898964+asiskaur@users.noreply.github.com>
2021-10-22 17:24:21 +0530

    Create house robber.cpp

---
+++ DP/house robber.cpp
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/house-robber/
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        int n=nums.size();
+        if(n==0)
+            return 0;
+        if(n==1)
+            return nums[0];
+        int dp[n];
+        
+        dp[0]=nums[0];
+        
+        dp[1]=max(nums[0],nums[1]);
+        
+        
+        
+        for(int i=2;i<n;i++){
+            
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        
+        return dp[n-1];
+        
+    }
+};
