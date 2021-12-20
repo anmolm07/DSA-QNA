e2e2ed4851b480ca5131f8cc6a9860f788cf5199
ANMOL MITTAL <79745096+anmolm07@users.noreply.github.com>
2021-12-20 17:11:08 +0530

    Update diameter of tree.cpp

--- Tree/diameter of tree.cpp
+++ Tree/diameter of tree.cpp
@@ -30,3 +30,32 @@ class Solution {
         // Your code here
     }
 };
+
+//UPDATED 
+
+
+class Solution {
+  public:
+    // Function to return the diameter of a Binary Tree.
+    int diameterOpt(struct Node* root,int &height){
+        int l = 0, r= 0;
+  
+        if (root == NULL) {
+            return 0; 
+        }
+        l = diameterOpt(root->left, height);
+        r = diameterOpt(root->right, height);
+  
+        int temp = max(l, r) + 1;
+        int ans=max(temp,1+l+r);
+        height= max(height,ans);
+        return temp;
+    }
+    int diameter(Node* root) {
+        int height=INT_MIN;
+        diameterOpt(root,height);
+        return height;
+        
+        // Your code here
+    }
+};
