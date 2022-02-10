3ed96ca3fddb4f4b6ff28cd71a9a453f2648118b
ANMOL MITTAL <79745096+anmolm07@users.noreply.github.com>
2022-02-11 00:13:45 +0530

    Create add-digit.cpp

---
+++ array/add-digit.cpp
@@ -0,0 +1,24 @@
+/*
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+
+LINK - https://leetcode.com/problems/add-digits/
+
+*/
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num==0)
+            return 0;
+        else if(num%9==0)
+            return 9;
+        else
+            return num%9;
+    }
+};
